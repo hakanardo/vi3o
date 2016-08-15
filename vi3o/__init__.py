64ab2d99aefaf35fed8095d4d367672f855155de
Hakan Ardo <hakan@cognimatics.com>
2016-08-15 11:13:18 +0200

    version

--- vi3o/__init__.py
+++ vi3o/__init__.py
@@ -3,7 +3,7 @@
 ====================================
 """
 
-__version_info__ = (0, 4, 1)
+__version_info__ = (0, 5, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
 def Video(filename, grey=False):
