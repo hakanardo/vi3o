19f4739ba57dae7f15ef5376f29b7ee993049661
Hakan Ardo <ardo@maths.lth.se>
2016-11-10 11:45:59 +0100

    release

--- vi3o/__init__.py
+++ vi3o/__init__.py
@@ -3,7 +3,7 @@
 ====================================
 """
 
-__version_info__ = (0, 5, 2)
+__version_info__ = (0, 6, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
 # FIXME: Turn into a Video base class that documents the interface
