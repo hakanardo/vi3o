1f3ebc51170d1741e980d64c375cfe9048d11913
Håkan x Ardö <hakanad@axis.com>
2022-09-14 14:16:36 +0200

    release

--- release.sh
+++ release.sh
@@ -4,11 +4,12 @@ if [ ! -z "`git diff`" ]; then
     echo Please commit first
     exit
 fi
-git tag v`PYTHONPATH=. python -c 'import vi3o; print vi3o.__version__' 2>/dev/null | tail -1` || exit
+TAG=v`PYTHONPATH=. python -c 'import vi3o; print (vi3o.__version__)' 2>/dev/null | tail -1`
+gh release create $TAG --title $TAG --notes "Release $TAG" || exit
 git checkout stable || exit
-tsocks git pull
+git pull
 git merge master || exit
-tsocks git push
-tsocks git push --tags
+git push
+git push --tags
 git checkout master
-tsocks git push
+git push
